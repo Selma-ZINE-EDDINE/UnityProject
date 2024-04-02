c786416b1604dd9de5bfa3824a54788a475619e6
smiranii <ichrak.smirani@edu.esiee.fr>
2024-04-02 15:06:14 +0200

    Function Player2

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput2 : BaseInput
+    {
+        public string TurnInputName = "Horizontal2";
+        public string AccelerateButtonName = "Accelerate2";
+        public string BrakeButtonName = "Brake2";
+
+        public override InputData GenerateInput() {
+            return new InputData
+            {
+                Accelerate = Input.GetButton(AccelerateButtonName),
+                Brake = Input.GetButton(BrakeButtonName),
+                TurnInput = Input.GetAxis("Horizontal")
+            };
+        }
+    }
+}
