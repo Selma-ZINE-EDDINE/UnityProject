454c3617d347392bd64006d5218a8d73e6c51bd4
Selma-ZINE-EDDINE <selma.zine-eddine@edu.esiee.fr>
2024-05-03 00:36:09 +0200

    winner

--- Assets/Karting/Scripts/GameModes/PickupObject.cs
+++ Assets/Karting/Scripts/GameModes/PickupObject.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This class inherits from TargetObject and represents a PickupObject.
@@ -15,11 +16,13 @@ public class PickupObject : TargetObject
     
     [Tooltip("Destroy this gameobject after collectDuration seconds")]
     public float collectDuration = 0f;
-
+    private int winner = 1;
     void Start() {
+        PlayerPrefs.SetInt("Winner", 1);
+        PlayerPrefs.Save();
         Register();
     }
-
+    public int Winner { get { return winner; } set {  winner = value; } }
     void OnCollect()
     {
         if (CollectSound)
@@ -42,9 +45,67 @@ public class PickupObject : TargetObject
     
     void OnTriggerEnter(Collider other)
     {
-        if ((layerMask.value & 1 << other.gameObject.layer) > 0 && other.gameObject.CompareTag("Player"))
+        if ((layerMask.value & 1 << other.gameObject.layer) > 0 && ( other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Player2")))
         {
             OnCollect();
+        }
+        if (gameObject.CompareTag("LastTag"))
+        {
+            //float newXPosition = -232.4f;
+            if (other.gameObject.CompareTag("Player2"))
+            {
+                //newXPosition = 237;
+                winner = 2;
+                PlayerPrefs.SetInt("Winner", 2);
+                PlayerPrefs.Save();
+                
+            }
+            /*// Trouver tous les objets avec le tag "winner"
+            GameObject[] winners = GameObject.FindGameObjectsWithTag("winner");
+
+            // Parcourir tous les objets gagnants et affecter la nouvelle position X
+            foreach (GameObject winner in winners)
+            {
+                // Récupérer la position actuelle de l'objet
+                Vector3 currentPosition = winner.transform.position;
+
+                // Affecter la nouvelle position avec la même valeur Y et Z mais avec la nouvelle position X
+                Vector3 newPosition = new Vector3(newXPosition, currentPosition.y, currentPosition.z);
+
+                // Définir la nouvelle position à l'objet
+                winner.transform.position = newPosition;*/
+            /*if (!(SceneManager.GetSceneByName("WinScene").isLoaded))
+            {
+                Debug.Log("Win Scene pas loaded");
+                SceneManager.LoadSceneAsync("WinScene", LoadSceneMode.Additive);
+            }
+            if (SceneManager.GetSceneByName("WinScene").isLoaded)
+            {
+                Debug.Log("Win Scene loaded");
+                // Récupérez tous les objets ayant le tag "winner"
+                GameObject[] winners = GameObject.FindGameObjectsWithTag("winner");
+
+                // Parcourez chaque objet et affectez-lui la position X souhaitée
+                foreach (GameObject winner in winners)
+                {
+                    // Assurez-vous que l'objet a un composant transform (qui contient la position)
+                    if (winner.GetComponent<Transform>() != null)
+                    {
+                        // Affectez la nouvelle position X
+                        Vector3 newPosition = winner.transform.position;
+                        //newPosition.x = desiredXPosition;
+                        winner.transform.position = newPosition;
+                        // Récupérer la position actuelle de l'objet
+                        Vector3 currentPosition = winner.transform.position;
+
+                        // Affecter la nouvelle position avec la même valeur Y et Z mais avec la nouvelle position X
+                        Vector3 newPosition = new Vector3(newXPosition, currentPosition.y, currentPosition.z);
+
+                        // Définir la nouvelle position à l'objet
+                        winner.transform.position = newPosition; 
+                    }
+                }
+            }*/
         }
     }
 }
---
+++ Assets/scripts/position_txt.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position_txt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+    //public PickupObject checkpoint;
+    public int WinnerPlayer = 1;
+    void Start()
+    {
+        WinnerPlayer = PlayerPrefs.GetInt("Winner");
+        if (WinnerPlayer == 1)
+        {
+            //Vector3 p = new Vector3(-232.4f, 147.34f, 0);
+            transform.position = new Vector3(220f, 400f, 0);
+        }
+        if (WinnerPlayer == 2)
+        {
+            Vector3 p = new Vector3(620f, 400f, 0);
+            this.transform.position = p;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
